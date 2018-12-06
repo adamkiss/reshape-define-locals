6abf8a3cc51f60dcb115a4d42ff759fc79f5cac7
Adam Kiss <i.am.adam.kiss+git@gmail.com>
2018-12-06 15:08:42 +0100

    Remove all modes, keep only YAML. Also changed options 'key' and 'delete' into single 'scope'

--- index.js
+++ index.js
@@ -8,25 +8,6 @@ function isDefinition(node, tag) {
 	return node.type === 'tag' && node.name === tag
 }
 
-function scopeEval(node, PluginError, fnString, locals = null) {
-	try {
-		// eslint-disable-next-line no-eval
-		return eval(fnString)(locals)
-	} catch (err) {
-		const location = ('location' in node) ? node.location : node.content[0].location
-
-		if (err.name === 'SyntaxError')
-			throw new PluginError({
-				message: `There was a SyntaxError in your Define Locals block of type='${locals ? 'function' : 'object'}'. Did you mean to use YAML type (default)?`,
-				plugin: 'reshape-define-locals', location
-			})
-		else
-			throw new PluginError({
-				message: err.toString(), plugin: 'reshape-define-locals', location
-			})
-	}
-}
-
 const emptyNode = {type: 'text', content: ''}
 
 const mergeWithCustomizer = (objVal, srcVal) => {
@@ -36,59 +17,28 @@ const mergeWithCustomizer = (objVal, srcVal) => {
 
 module.exports = function reshapeDefineLocals(opts) {
 	const options = Object.assign({
-		mode: 'yaml',
 		tag: 'define-locals',
-		key: 'locals',
-		delete: false
+		scope: 'file'
 	}, opts)
 
 	return function defineLocalsPlugin(tree, ctx) {
-		if (options.delete && options.key in ctx.locals)
-			delete ctx.locals[options.key]
+		if (options.scope && options.key in ctx.locals)
+			delete ctx.locals[options.scope]
 
 		return modifyNodes(tree, node => isDefinition(node, options.tag), node => {
-			let {mode} = options
-
 			// if node.location is defined, we will prefer innerHTML (= probably HTML)
 			// Otherwise content will be sufficient (probably it's SugarML)
 			const contentRaw = 'location' in node ?
 				node.location.innerHTML :
 				node.content[0].content
-
-			// normalize content and convert to spaces (for yaml)
 			const content = convertToSpaces(stripIndent(contentRaw), 2)
-
-			if ('attrs' in node && 'type' in node.attrs)
-				mode = node.attrs.type[0].content
-
-			let definedLocals = {}
-			switch (mode) {
-				case 'yaml':
-					definedLocals = yaml.safeLoad(content, 'utf8')
-					break
-				case 'function':
-					definedLocals = scopeEval(
-						node, ctx.PluginError,
-						`locals => {${content}}`, options.locals
-					)
-					break
-				case 'object':
-					// eslint-disable-next-line no-eval
-					definedLocals = scopeEval(
-						node, ctx.PluginError,
-						`_ => { return {${content}}}`
-					)
-					break
-				default:
-					throw new ctx.PluginError({
-						message: `"${mode}" isn't a valid type of "define-locals" block`,
-						plugin: 'reshape-define-locals',
-						location: node.location
-					})
-			}
+			const definedLocals = yaml.safeLoad(content, 'utf8')
 
 			// deep merge with array concat customization
-			mergeWith(ctx.locals, {[options.key]: definedLocals}, mergeWithCustomizer)
+			if (options.scope)
+				mergeWith(ctx.locals, {[options.scope]: definedLocals}, mergeWithCustomizer)
+			else
+				mergeWith(ctx.locals, definedLocals, mergeWithCustomizer)
 
 			return emptyNode
 		})
